7e353ed7059d2da19b85937181df212908f29c0e
Allen Kim <Allenkim5838@gmail.com>
2024-02-12 00:07:55 -0500

    Set Up DB and User Schemas

---
+++ controllers/auth.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcrypt";
+import jwc from "jsonwebtoken";
+import user from "../models/user.js";
+
+/* REGISTER USER */ 
+export const register = async (req, res) => {
+    
+}
--- index.js
+++ index.js
@@ -9,7 +9,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import { register } from "module";
+import { register } from "./controllers/auth.js";
+ 
 /* CONFIGS */
 //Grab file URL
 const __filename = fileURLToPath(import.meta.url);
@@ -43,3 +45,16 @@ const storage = multer.diskStorage({
     },
 });
 const upload = multer({ storage });
+
+/* ROUTES for files */
+app.post("/auth/register" , upload.single("picture"), register);
+
+/* MONGOOSE SET */
+const PORT = process.env.PORT || 6001;
+mongoose
+.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true;
+    useUnifiedTopology: true;
+}).then(() => {
+    app.listen(PORT, () => console.log('Server Port: ${PORT}'));
+}).catch((error) => console.log(`${error} did not connect`));
