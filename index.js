f64cc57e3f5d8fdc42d5eaa5c099198e80d50e69
Allen Kim <Allenkim5838@gmail.com>
2024-03-05 21:21:50 -0500

    fix syntax

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ app.post("/auth/register" , upload.single("picture"), register);
 const PORT = process.env.PORT || 6001;
 mongoose
 .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true;
-    useUnifiedTopology: true;
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 }).catch((error) => console.log(`${error} did not connect`))
